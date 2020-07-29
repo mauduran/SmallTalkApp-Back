671be282f173c19727b6b737ccf9900d020f45e2
Duran <duran@MBP-Duran.local>
2020-07-28 22:09:56 -0500

    Added get methods for all users and a single user

---
+++ Endpoints/Users.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const router = express.Router();
+
+const dummyUsers = require('../Dummies/dummyUsers');
+
+router.route('/')
+    .get((req, res) => {
+        const searchInput = req.query.searchInput.toLowerCase();
+        const users = dummyUsers.getDummyUsers();
+
+        const usersFound = users.filter(user => user.name.toLowerCase().includes(searchInput)
+        || user.username.toLowerCase().includes(searchInput));
+
+        res.json(usersFound);
+    });
+
+router.route('/:userId')
+    .get((req, res) => {
+        const userId = req.params.userId;
+
+        const users = dummyUsers.getDummyUsers();
+
+        const user = users.find(usr => usr.userId==userId);
+
+        console.log(user);
+
+        res.json(user);
+    });
+
+module.exports = router; 
