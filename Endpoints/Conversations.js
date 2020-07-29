d5d9f4281eb0365678c35e44e232cc9dacf66544
Duran <duran@MBP-Duran.local>
2020-07-28 22:09:34 -0500

    Added get method for all conversations

--- Endpoints/Conversations.js
+++ Endpoints/Conversations.js
@@ -3,13 +3,47 @@ const router = express.Router();
 
 const dummyConversations = require('../Dummies/dummyConversations');
 
+
+router.route('/')
+    .post((req, res) => {
+        console.log(req.body);
+        let newId = -1;
+
+        const convos = dummyConversations.getDummyConversations();
+        convos.forEach(element => {
+            if(element.conversationId>newId) newId = element.conversationId;
+        });
+
+        newId++;
+
+        const newConversation = {
+            ...req.body, 
+            conversationId: newId,
+            lastMessage: {
+                body: "",
+                date: new Date(),
+                sender: ""
+            }
+        }
+
+        dummyConversations.updateDummyConversations([...convos, newConversation]);
+
+        console.log(newConversation);
+
+        res.json(newConversation);
+
+    });
+
 router.route('/:username')
-    .get((req, res)=>{
-        const conversations = dummyConversations.getDummyConversations();
+    .get((req, res) => {
+        const conversations = dummyConversations.getDummyConversations().sort((a,b)=>{
+            console.log(a.lastMessage.date - b.lastMessage.date);
+            return b.lastMessage.date - a.lastMessage.date;
+        });
 
-        res.json(conversations.filter(el=>el.members && el.members.includes(req.params.username)));
+        res.json(conversations.filter(el => el.members && el.members.includes(req.params.username)));
 
-        
+
     });
 
 
