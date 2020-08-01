135ae21ee27c9bf899a8c63481db3a9493916ce6
Duran <duran@MBP-Duran.local>
2020-07-31 22:48:22 -0500

    changed roomId to conversationId for consistency

--- Dummies/dummyMessages.js
+++ Dummies/dummyMessages.js
@@ -1,48 +1,48 @@
 const dummyMessages = [
     {
-        roomId: 1,
+        conversationId: 1,
         type: 'message',
         body: "Hola, cómo estás?",
         date: new Date(2019, 6, 5, 12, 24, 0),
         sender: "mau4duran"
     },
     {
-        roomId: 1,
+        conversationId: 1,
         type: 'message',
         body: "Qué tal, buen hombre.",
         date: new Date(2020, 6, 4, 14, 14, 30),
         sender: "jprr44"
     },
     {
-        roomId: 1,
+        conversationId: 1,
         type: 'message',
         body: "Cómo vamos con el sitio?",
         date: new Date(2020, 6, 5, 14, 34, 0),
         sender: "mau4duran"
     },
     {
-        roomId: 1,
+        conversationId: 1,
         type: 'message',
         body: "Bien",
         date: new Date(2020, 6, 5, 14, 35, 0),
         sender: "jprr44"
     },
     {
-        roomId: 1,
+        conversationId: 1,
         type: 'message',
         body: "Falta lo de mensajes.",
         date: new Date(2020, 6, 5, 12, 35, 20),
         sender: "jprr44"
     },
     {
-        roomId: 1,
+        conversationId: 1,
         type: 'message',
         body: "Voy",
         date: new Date(2020, 6, 5, 12, 44, 0),
         sender: "mau4duran"
     },
     {
-        roomId: 2,
+        conversationId: 2,
         type: 'message',
         body: "Voy",
         date: new Date(2020, 6, 5, 12, 44, 0),
@@ -50,7 +50,7 @@ const dummyMessages = [
     },
 
     {
-        roomId: 3,
+        conversationId: 3,
         type: 'message',
         body: "qué pedo",
         date: new Date(2020, 6, 5, 10, 40, 0),
@@ -58,14 +58,14 @@ const dummyMessages = [
     },
 
     {
-        roomId: 4,
+        conversationId: 4,
         type: 'message',
         body: "heyyy",
-        date: new Date(2020, 7, 5, 19, 40, 0),
+        date: new Date(2020, 6, 5, 19, 40, 0),
         sender: "jsfran"
     },
     {
-        roomId: 4,
+        conversationId: 4,
         type: 'leave-conversation',
         body: "",
         date: new Date(),
